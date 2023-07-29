813a84e193230f10433f131f4fc9e1a055f3358b
pkothuru <priyatha.kothuru@sjsu.edu>
2023-07-28 17:44:32 -0700

    new

--- new.py
+++ new.py
@@ -1,30 +1,18 @@
 import git
 import os
-import yaml
 
-def get_yaml_changes(repo_path, yaml_file_path):
+def get_added_lines_in_yaml(repo_path, yaml_file_path):
     repo = git.Repo(repo_path)
-    commits = list(repo.iter_commits(max_count=2))
+    head_commit = repo.head.commit
 
-    if len(commits) < 2:
-        print("Error: Not enough commits in the repository.")
-        return None
+    diff = repo.git.diff(head_commit.parents[0], head_commit, "--", yaml_file_path)
 
-    commit1 = commits[1]
-    commit2 = commits[0]
+    added_lines = []
+    for line in diff.splitlines():
+        if line.startswith("+") and not line.startswith("+++"):
+            added_lines.append(line.strip("+ ").strip())
 
-    diff = repo.git.diff(commit1, commit2, "--name-only")
-    changed_files = diff.splitlines()
-
-    if yaml_file_path in changed_files:
-        yaml_changes = repo.git.diff(commit1, commit2, "--", yaml_file_path)
-        return yaml_changes
-    else:
-        return None
-
-def load_yaml_file(file_path):
-    with open(file_path, "r") as file:
-        return yaml.safe_load(file)
+    return added_lines
 
 if __name__ == "__main__":
     repo_path = "/Users/priyatha.kothuru/Desktop/pyyam"  # Replace this with the path to your Git repository
@@ -38,24 +26,13 @@ if __name__ == "__main__":
         print(f"Error: YAML file '{yaml_file_path}' does not exist.")
         exit(1)
 
-    yaml_changes = get_yaml_changes(repo_path, yaml_file_path)
-    if yaml_changes is None:
-        print("No differences found in the 'name' field of the YAML file between the last two commits.")
-    else:
-        print("Changes in the YAML file between the last two commits:")
-        print(yaml_changes)
-
-        # Extract and print the names of the differences from the Git diff
-        names_of_differences = []
-        for line in yaml_changes.splitlines():
-            if line.strip().startswith("- name: "):
-                name = line.strip().split(": ")[1]
-                names_of_differences.append(name)
-
-        print("Names of the differences:")
-        print(names_of_differences)
-
-        # Optionally, you can also load and print the YAML content for the latest commit:
-        latest_yaml_content = load_yaml_file(yaml_file_path)
-        print("Latest YAML content:")
-        print(yaml.dump(latest_yaml_content, default_flow_style=False))
+    try:
+        added_lines = get_added_lines_in_yaml(repo_path, yaml_file_path)
+        if not added_lines:
+            print("No newly added lines found in the YAML file between the working directory and the latest commit.")
+        else:
+            print("Newly added lines in the YAML file between the working directory and the latest commit:")
+            for line in added_lines:
+                print(line)
+    except git.GitCommandError as e:
+        print(f"Error: {e}")
