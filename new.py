74aa52a50d50dc05f08e96f690335841def59fdc
pkothuru <priyatha.kothuru@sjsu.edu>
2023-07-28 17:58:14 -0700

    new add

--- new.py
+++ new.py
@@ -1,18 +1,22 @@
 import git
 import os
 
-def get_added_lines_in_yaml(repo_path, yaml_file_path):
+def get_added_names_in_yaml(repo_path, yaml_file_path):
     repo = git.Repo(repo_path)
     head_commit = repo.head.commit
 
     diff = repo.git.diff(head_commit.parents[0], head_commit, "--", yaml_file_path)
 
-    added_lines = []
+    added_names = []
+    in_added_block = False
+
     for line in diff.splitlines():
         if line.startswith("+") and not line.startswith("+++"):
-            added_lines.append(line.strip("+ ").strip())
+            stripped_line = line.strip("+ ").strip()
+            if stripped_line.startswith("name: "):
+                added_names.append(stripped_line.split(": ", 1)[1])
 
-    return added_lines
+    return added_names
 
 if __name__ == "__main__":
     repo_path = "/Users/priyatha.kothuru/Desktop/pyyam"  # Replace this with the path to your Git repository
@@ -27,11 +31,12 @@ if __name__ == "__main__":
         exit(1)
 
     try:
-        added_lines = get_added_lines_in_yaml(repo_path, yaml_file_path)
-        if not added_lines:
-            print("No newly added lines found in the YAML file between the working directory and the latest commit.")
+        added_names = get_added_names_in_yaml(repo_path, yaml_file_path)
+        if not added_names:
+            print("No newly added 'name' fields found in the YAML file between the working directory and the latest commit.")
         else:
-            print("Newly added lines in the YAML file between the working directory and the latest commit:")
-        print(len(added_lines))
+            print("Newly added 'name' fields in the YAML file between the working directory and the latest commit:")
+            for name in added_names:
+                print(name)
     except git.GitCommandError as e:
         print(f"Error: {e}")
